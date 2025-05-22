9cc210cadeb5635313813e1a244966d67eb39a3b
Krzysztof Furman <k_fuman@outlook.com>
2025-05-22 21:40:32 +0100

    Core tools.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# kitty terminal
+sudo dnf install kitty -y #and then get the catpuccin theme from https://github.com/catppuccin/kitty
+
+# git
+sudo dnf install git -y
+
+# pip
+sudo dnf install python3-pip -y
+
+# vs-code
+# works better from their website .rpm than from command line somehow
+
+# chrome
+# also get .rpm from their website
+
+# rust
+sudo dnf install curl gcc make -y
+
+# C++
+sudo dnf install gcc-c++ -y
+
+# node-js - usally already installed tho
+sudo dnf install nodejs -y
+
+# neovim
+sudo dnf install neovim -y #might need to edit kitty config to make it default
+
